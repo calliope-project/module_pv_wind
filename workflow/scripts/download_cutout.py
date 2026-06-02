2c9f7396198030c19425d37ddf208c0e85e97e18
Jann Launer <32454596+jnnr@users.noreply.github.com>
2026-06-02 17:25:05 +0200

    Wrap download into function

--- workflow/scripts/download_cutout.py
+++ workflow/scripts/download_cutout.py
@@ -7,17 +7,25 @@ import atlite
 logger = logging.getLogger(__name__)
 
 
-if __name__ == "__main__":
-    logger.info(f"Using atlite version: {atlite.__version__}")
-    cutout_params = snakemake.config["cutout_params"]
-
+def download_cutout(cutout_path, cutout_params):
+    """Downloads a cutout based on the provided parameters."""
     cutout_params["x"] = slice(*cutout_params["x"])
     cutout_params["y"] = slice(*cutout_params["y"])
     cutout_params["time"] = slice(*cutout_params["time"])
 
     features = cutout_params.pop("features", None)
     monthly_requests = cutout_params.pop("monthly_requests", False)
+
     logger.info(f"Preparing cutout with cutout_params: {cutout_params}")
     logger.info(f"Preparing cutout with features: {features}")
-    cutout = atlite.Cutout(snakemake.output[0], **cutout_params)
+
+    cutout = atlite.Cutout(cutout_path, **cutout_params)
     cutout.prepare(features=features, monthly_requests=monthly_requests)
+
+
+if __name__ == "__main__":
+    logger.info(f"Using atlite version: {atlite.__version__}")
+
+    download_cutout(
+        cutout_path=snakemake.output[0], cutout_params=snakemake.config["cutout_params"]
+    )
